dde550cc3d53ccd30457ca6e7906b3659c93c7f9
alastairknowles <alastair@prism.hr>
2017-01-09 18:22:50 +0000

    Tidy up.

--- src/main/java/uk/co/comment/relational/repository/CommentRepository.java
+++ src/main/java/uk/co/comment/relational/repository/CommentRepository.java
@@ -14,8 +14,8 @@ public interface CommentRepository extends CrudRepository<Comment, Long> {
     @Query(value = "select new uk.co.comment.relational.rest.CommentDTO(c.id, c.comment, c.name, c.posted, count(cl.id)) " +
             "from Comment c " +
             "left join c.likes cl " +
-            "group by c.id order " +
-            "by c.id desc")
+            "group by c.id " +
+            "order by c.id desc")
     List<CommentDTO> findAllWithLikeCountsOrderByIdDesc();
     
 }
